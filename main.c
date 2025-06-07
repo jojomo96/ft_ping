dfb7c41f2dacecad9f15a3e5d1f68b685e9c0631
Johannes Moritz <johannes.moritz@stackit.cloud>
2025-06-08 00:20:11 +0200

    update ft_getopt to support optional arguments and adjust main function accordingly

--- main.c
+++ main.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
+#include "libft/ft_getopt.h"
 
-int ft_getopt(int argc, char ** argv, char * str);
-
-char * opt_arg;
-
-int opt_index;
-
-int main(int argc, char **argv) {
+int main(int argc, char *argv[])
+{
     int c;
-    while ((c = ft_getopt(argc, argv, "abo:")) != -1) {
+
+    while ((c = ft_getopt(argc, argv, "abo:c::")) != -1) {
         switch (c) {
-            case 'a': puts("saw -a");          break;
-            case 'b': puts("saw -b");          break;
-            case 'o': printf("-o value = %s\n", opt_arg); break;
-            case '?':                         /* unknown switch   */
-                /* tiny_getopt already printed a message */
+            case 'a':
+                puts("saw -a");
+                break;
+            case 'b':
+                puts("saw -b");
+                break;
+            case 'o':
+                printf("-o value = %s\n", ft_optarg);
+                break;
+            case 'c':
+                if (ft_optarg)
+                    printf("-c optional arg = %s\n", ft_optarg);
+                else
+                    puts("-c with no arg");
+                break;
+            case '?':
                 return 1;
-            case ':':                         /* missing argument */
-                /* message already printed                        */
+            case ':':
                 return 1;
         }
     }
 
-    /* Remaining arguments start at tiny_optind */
-    for (int i = opt_index; i < argc; ++i)
-        printf("non-option argument: %s\n", argv[i]);
+    for (int i = ft_optind; i < argc; ++i)
+        printf("non-option arg: %s\n", argv[i]);
 
     return 0;
 }
